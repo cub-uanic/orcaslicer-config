93f34909c99e881b81a200679df3769e0da3f1c1
Oleg Kostyuk <cub.uanic@gmail.com>
2026-01-04 03:02:41 +0100

    update

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # crontab -e
+# # vi:ft=crontab
+# MAILTO=cub.uanic@gmail.com
+#
 #
 # m     h       dom     mon     dow     command
 # */15  *       *       *       *       ~/Library/Application\ Support/OrcaSlicer/backup.sh >/dev/null
@@ -9,9 +12,9 @@ SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 cd "$SCRIPT_DIR"
 
-git add -A
-git commit -m update
-git fetch --all
-git pull
-git push
+git add -A >/dev/null
+git commit --quiet -m update
+git fetch --quiet --all
+git pull --quiet
+git push --quiet
 
